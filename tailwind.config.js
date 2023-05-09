3d49bccaca001ab4d87a506da96f4d17df476665
florian-glanum <florian@glanum.com>
2023-05-09 10:58:09 +0200

    feat(dark): test theme color and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
   theme: {
+    colors: {
+      primary: "rgb(var(--color-primary) / <alpha-value>)",
+      white: "rgb(var(--color-white) / <alpha-value>)",
+      text: "rgb(var(--color-text) / <alpha-value>)",
+      light: "rgb(var(--color-light) / <alpha-value>)",
+      success: "rgb(var(--color-success) / <alpha-value>)",
+      info: "rgb(var(--color-info) / <alpha-value>)",
+      warn: "rgb(var(--color-warn) / <alpha-value>)",
+      error: "rgb(var(--color-error) / <alpha-value>)",
+      transparent: "transparent",
+      current: "currentColor",
+    },
     extend: {},
   },
   plugins: [
